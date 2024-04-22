688bee401f609a02953862d9064fceb6018030e8
Abu_Saker <hamza.damra@students.alquds.edu>
2024-04-22 18:33:48 +0300

    Third Commit

---
+++ app/src/main/java/com/hamza/newsapp/SourceListTypeToken.java
@@ -0,0 +1,9 @@
+package com.hamza.newsapp;
+
+import com.google.gson.reflect.TypeToken;
+import com.hamza.newsapp.model.Source;
+
+import java.util.ArrayList;
+
+public class SourceListTypeToken extends TypeToken<ArrayList<Source>> {}
+
